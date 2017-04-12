fd094e669fbfd8257e69ce4e25cdcdb8c9113ca5
Ashok Reddy Narra <anarra@innominds.com>
2017-04-12 20:54:03 +0530

    updated project with hello world

--- src/com/example/androidsamples/CallbackInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.androidsamples;
-
-public interface CallbackInterface {
-	public void aNotifyServiceDisconnected();
-	public void aShowConnectButton();
-	public void  aNotifyServiceConnected();
-	public void  aShowDisconnectButton();
-
-}
